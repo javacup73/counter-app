7f067d3581c0ea19af1d91d8aeab7cf72c403af4
Kalyan Dasika <kalyan@goldblattsystems.com>
2021-10-30 16:47:18 -0700

    handle state changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
-    count: 1,
+    count: 0,
     tags: ["tag1", "tag2", "tag3"],
   };
   style = {
@@ -26,7 +26,10 @@ class Counter extends React.Component {
   }
   //has the same effect as the constructor, the reference to this is not undefined
   handleIncrement = () => {
-    console.log("Increment Clicked: " + this);
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
   };
   render() {
     return (
@@ -40,13 +43,26 @@ class Counter extends React.Component {
         >
           Increment
         </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge alert-";
-    classes += this.state.count == 0 ? "warning" : "primary";
+    switch (this.state.count) {
+      case 0:
+        classes += "warning";
+        break;
+      default:
+        classes += this.state.count > 0 ? "primary" : "danger";
+        break;
+    }
     return classes;
   }
 
