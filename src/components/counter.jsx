b2f19fe067d108c5e0cca476c70621c3fe7fadbe
Kalyan Dasika <kalyan@goldblattsystems.com>
2021-10-30 19:33:14 -0700

    how to make child component react to events handled in parent components?

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,37 +1,37 @@
 import React, { Component } from "react";
-
+//changing this to a controlled child component which means the parent controls the state and behavior of this component
 class Counter extends React.Component {
   //difference between state and props:
   //state: is internal to the component (private data)
   //props: is data that you can pass into the component from another component
   //external components do not have access to internal state of other components
   //components cannot modify the values passed into the component from other components (immutable)
-  state = {
-    value: this.props.counter.value,
-    tags: ["tag1", "tag2", "tag3"],
-  };
+  //   state = {
+  //     // value: this.props.counter.value,
+  //     tags: ["tag1", "tag2", "tag3"],
+  //   };
   style = {
     fontSize: 10,
     fontWeight: "bold",
     color: "black",
   };
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return <p>There are no tags</p>;
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map((tag) => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     );
+  //   }
   //has the same effect as the constructor, the reference to this is not undefined
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-  handleDecrement = () => {
-    this.setState({ value: this.state.value - 1 });
-  };
+  //   handleIncrement = () => {
+  //     this.setState({ value: this.state.value + 1 });
+  //   };
+  //   handleDecrement = () => {
+  //     this.setState({ value: this.state.value - 1 });
+  //   };
 
   render() {
     return (
@@ -40,20 +40,20 @@ class Counter extends React.Component {
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => {
-            this.handleIncrement({ id: 1 });
+            this.props.onIncrement(this.props.counter);
           }}
           className="btn btn-secondary btn-sm m-2"
         >
           Increment
         </button>
         <button
-          onClick={this.handleDecrement}
+          onClick={() => this.props.onDecrement(this.props.counter)}
           className="btn btn-secondary btn-sm m-2"
         >
           Decrement
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => this.props.onDelete(this.props.counter)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -64,20 +64,19 @@ class Counter extends React.Component {
 
   getBadgeClasses() {
     let classes = "badge alert-";
-    switch (this.state.value) {
+    switch (this.props.counter.value) {
       case 0:
         classes += "warning";
         break;
       default:
-        classes += this.state.value > 0 ? "primary" : "danger";
+        classes += this.props.counter.value > 0 ? "primary" : "danger";
         break;
     }
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
-    return count == 0 ? "Zero" : count;
+    return this.props.counter.value == 0 ? "Zero" : this.props.counter.value;
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,9 +10,9 @@ class Counters extends React.Component {
       { id: 4, value: 3 },
     ],
   };
-  handleDelete = (counterId) => {
+  handleDelete = (counter) => {
     // console.log("Delete event handler called", counterId);
-    const counters = this.state.counters.filter((c) => c.id != counterId);
+    const counters = this.state.counters.filter((c) => c.id != counter.id);
     //this works too!
     // this.setState({ counters: counters });
     this.setState({ counters });
@@ -22,6 +22,28 @@ class Counters extends React.Component {
       c.value = 0;
       return c;
     });
+    this.setState({ counters });
+  };
+  handleIncrement = (counter) => {
+    //spread operator ... clone of state
+    const counters = [...this.state.counters];
+    //get the index of the counter being modified
+    const index = counters.indexOf(counter);
+    //we don't want to directly modify the state (a big no no in React) so need to clone the counter object at the location of incoming counter and then
+    //set the state of the array
+    //e.g. counters[0].value++ will modify the state directly which is not allowed ... in other words, react does not update the DOM
+    // we only need to clone the entry that we need to modify thats the reason for getting the index
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDecrement = (counter) => {
+    //spread operator ... clone of state
+    const counters = [...this.state.counters];
+    //get the index of the counter being modified
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
     this.setState({ counters });
   };
   render() {
@@ -40,6 +62,8 @@ class Counters extends React.Component {
             // value={counter.value}
             // id={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
           />
         ))}
       </div>
